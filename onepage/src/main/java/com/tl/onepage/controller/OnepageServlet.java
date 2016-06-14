7a671bcebaf870402627ef0b3b5e563464e5d0fc
terrytao <taoli51@gmail.com>
2016-06-14 19:29:52 +0800

    update the idea project

---
+++ onepage/src/main/java/com/tl/onepage/controller/OnepageServlet.java
@@ -0,0 +1,17 @@
+package com.tl.onepage.controller;
+
+import java.io.IOException;
+
+/**
+ * Created by litao on 6/14/2016.
+ */
+@javax.servlet.annotation.WebServlet(name = "OnepageServlet")
+public class OnepageServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        super.doPost(request,response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        super.doGet(request, response);
+    }
+}
